f9e2756eb2493015eb46588adc747acae5228629
Paul Duszak <duszak.paul@gmail.com>
2019-06-19 02:14:30 -0500

    Add base HTML structure to App component

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+     <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#home">Filming In Chicago</a>
+      </nav>
+      <main className="container-fluid">
+        <div className="row">
+          <div className="col-md-6">
+            Col A
+          </div>
+          <div className="col-md-6">
+            Col B
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
